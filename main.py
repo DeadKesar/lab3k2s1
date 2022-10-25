539b0500b4261d7b609c4b76bd2c5145f3cadb98
DeadKesar <DeadK@DESKTOP-6J5VCO6>
2022-10-25 23:33:59 +0300

    add regex

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ import re
 if __name__ == '__main__':
     r = requests.get('https://edu.stankin.ru/mod/assign/view.php?id=324317')
     html = r.text
-    print(html)
+    res = re.findall(
+        r'\b(?:https|http|www)[\.\:\/]{1,3}[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(?:\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?',
+        html)
+    for i in res:
+        print(i)
